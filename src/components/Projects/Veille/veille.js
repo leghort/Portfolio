e8dd50554fdb7607279663dd6668545d6b056b7a
medaey <medaey@hotmail.com>
2023-12-30 08:09:43 +0200

    fix render pdf cv and veille

--- src/components/Projects/Veille/veille.js
+++ src/components/Projects/Veille/veille.js
@@ -1,45 +1,33 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import pdf from "../../../Assets/pdf/veilleTechnologique.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-function Ansible() {
-  const [width, setWidth] = useState(1200);
-  const [numPages, setNumPages] = useState(null);
-
-  useEffect(() => {
-    setWidth(window.innerWidth);
-  }, []);
-
+function ResumeNew() {
   return (
+    
     <div>
     <Container fluid className="resume-section">
-    <Row style={{ justifyContent: "center", position: "relative" }}>
+      <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button variant="primary" href={pdf} target="_blank">
             <AiOutlineDownload />
             &nbsp;Télécharger
           </Button>
-        </Row>
-        
-      <Row className="resume">
-          <Document
-    file={pdf}
-    onLoadSuccess={({ numPages })=>setNumPages(numPages)}
->
-    {Array.apply(null, Array(numPages))
-    .map((x, i)=>i+1)
-    .map(page => <Page pageNumber={page} scale={width > 786 ? 1.7 : 0.6}/>)}
-</Document>
       </Row>
-      <Row style={{ justifyContent: "center", position: "relative" }}>
+
+      <Row className="resume">
+        <object 
+        style={{ justifyContent: "center", position: "relative"}}
+        width="80%"
+        height="4600"
+        data={pdf}
+        type="application/pdf">
+      </object>
       </Row>
     </Container>
   </div>
   );
 }
 
-export default Ansible;
+export default ResumeNew;
--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,36 +1,32 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import pdf from "../../Assets/pdf/20230630_CossuMederic_CV.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
-  const [width, setWidth] = useState(1200);
-
-  useEffect(() => {
-    setWidth(window.innerWidth);
-  }, []);
-
   return (
+    
     <div>
-      <Container fluid className="resume-section">
-        <Row style={{ justifyContent: "center", position: "relative" }}>
+    <Container fluid className="resume-section">
+      <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button variant="primary" href={pdf} target="_blank">
             <AiOutlineDownload />
             &nbsp;Télécharger
           </Button>
-        </Row>
+      </Row>
 
-        <Row className="resume">
-          <Document file={pdf}>
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-        </Row>
-      </Container>
-    </div>
+      <Row className="resume">
+        <object 
+        style={{ justifyContent: "center", position: "relative"}}
+        width="80%"
+        height="4600"
+        data={pdf}
+        type="application/pdf">
+      </object>
+      </Row>
+    </Container>
+  </div>
   );
 }
 

25f3b7b423938b9479877b8b5918c0971bf11417
medaey <medaey@hotmail.com>
2023-12-30 08:38:19 +0200

    pdf to embed

--- src/components/Projects/Veille/veille.js
+++ src/components/Projects/Veille/veille.js
@@ -10,13 +10,13 @@ function ResumeNew() {
     <div>
     <Container fluid className="resume-section">
       <Row className="resume">
-        <object 
+        <embed 
         style={{ justifyContent: "center", position: "relative"}}
         width="80%"
         height="4600"
         data={pdf}
         type="application/pdf">
-      </object>
+      </embed>
       </Row>
     </Container>
   </div>
